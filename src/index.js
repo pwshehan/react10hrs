ee14f1842552baa88172c0ff7628466e47b9d4df
Shehan Pramitha <pwshehan@gmail.com>
2021-01-08 13:10:07 +0530

    working with components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,36 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function BookList() {
-  return <section></section>;
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
